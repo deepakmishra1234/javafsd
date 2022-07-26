93982c4c0701b86eb68cd099523a60c5b958e17a
deepakmishra1234 <109963531+deepakmishra1234@users.noreply.github.com>
2022-07-26 19:24:49 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,23 @@
+package phase1.project;
+
+public class Array
+{
+
+	public static void main(String[] args)
+	{
+		int [] marks = new int[3];
+		marks[0] = 97;// physics
+		marks[1] = 90;// chemistry
+		marks[2] = 98;// math
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		
+		
+		
+		
+
+	}
+
+}
+ 
